ae83367d40cbc51820012b530f54bb6362b88ef2
gaurang98671 <56935661+gaurang98671@users.noreply.github.com>
2021-06-17 22:30:14 +0530

    ..

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,15 @@
 variable "resource_grp" {
     type = string
     description = "Resource group name?"
+
 }
 
 variable "cluster_name" {
     type = string
     description = "Cluster name?"
+}
+
+variable "location" {
+    type = string
+    default = "Central India"
 }
